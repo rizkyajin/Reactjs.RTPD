146927319641d520d99b207169e96622ccae5096
rizkyajin <nugraha.rizky@gmail.com>
2021-03-24 19:52:18 +0700

    initial commit
    initial release

---
+++ build/precache-manifest.904fa2746da5b7d99d21335724f2e948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bb816a836f5e14b5d297212c62721c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c20af31d97545a8e11b",
+    "url": "/static/css/main.474ddbc6.chunk.css"
+  },
+  {
+    "revision": "1df55bedf377f348f1da",
+    "url": "/static/js/2.fced7d2a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fced7d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c20af31d97545a8e11b",
+    "url": "/static/js/main.2375c9dc.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
